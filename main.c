06fed3ace7b5d129e89c257c85847ba96bfb4337
SkyDogzz <thomas.stephan@live.fr>
2024-08-13 09:12:28 +0200

    Print bytes / bit size

--- main.c
+++ main.c
@@ -1,34 +1,29 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 
 #define BOOL_TAB_SIZE 64
 
-void boot_tab_init(bool *bool_tab, int size)
-{
+void boot_tab_init(bool *bool_tab, int size) {
   srand(time(NULL));
-  for(int i = 0; i < BOOL_TAB_SIZE; i++)
-  {
+  for (int i = 0; i < BOOL_TAB_SIZE; i++) {
     bool_tab[i] = rand() % 2;
   }
 }
 
-void print_err(char *str)
-{
-  fprintf(stderr, str, strlen(str));
-}
+void print_err(char *str) { fprintf(stderr, str, strlen(str)); }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   bool *bool_tab = malloc(sizeof(bool) * BOOL_TAB_SIZE);
-  if(!bool_tab)
-  {
+  if (!bool_tab) {
     print_err("Memory allocation failed !\n");
     return EXIT_FAILURE;
   }
   boot_tab_init(bool_tab, BOOL_TAB_SIZE);
+  printf("The Boolean Array is %d bytes or %d bit.\n", BOOL_TAB_SIZE,
+         (int)sizeof(bool) * 8 * BOOL_TAB_SIZE);
 
   free(bool_tab);
 
