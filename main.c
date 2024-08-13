adc0a054aab4d8d60cb154f88bf8ebcb113020fe
SkyDogzz <thomas.stephan@live.fr>
2024-08-13 10:24:58 +0200

    Use args for bool tab size

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include <string.h>
 #include <time.h>
 
-#define BOOL_TAB_SIZE 72
+#define BOOL_TAB_SIZE 64
 #define bool_8 char
 
-bool *bool_tab_init(void) {
-  bool *bool_tab = malloc(sizeof(bool) * BOOL_TAB_SIZE);
+bool *bool_tab_init(int size) {
+  bool *bool_tab = malloc(sizeof(bool) * size);
   srand(time(NULL));
-  for (int i = 0; i < BOOL_TAB_SIZE; i++) {
+  for (int i = 0; i < size; i++) {
     bool_tab[i] = rand() % 2;
   }
   return bool_tab;
@@ -18,7 +18,7 @@ bool *bool_tab_init(void) {
 
 bool_8 *bool_tab_pressed_init(bool *bool_tab, int size, int *pressed_size) {
   bool_8 *bool_tab_pressed;
-  *pressed_size = (sizeof(bool) * 2 * BOOL_TAB_SIZE + 7) / 8;
+  *pressed_size = (sizeof(bool) * 2 * size + 7) / 8;
   bool_tab_pressed = malloc(sizeof(bool_8) * *pressed_size);
   return bool_tab_pressed;
 }
@@ -26,21 +26,33 @@ bool_8 *bool_tab_pressed_init(bool *bool_tab, int size, int *pressed_size) {
 void print_err(char *str) { fprintf(stderr, str, strlen(str)); }
 
 int main(int argc, char **argv) {
-  bool *bool_tab = bool_tab_init();
+  int size = BOOL_TAB_SIZE;
+
+  if (argc > 1) {
+    size = atoi(argv[1]);
+    if (size <= 0) {
+      print_err("Invalid size provided, must be a positive integer.\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  bool *bool_tab = bool_tab_init(size);
   if (!bool_tab) {
     print_err("Memory allocation failed !\n");
     return EXIT_FAILURE;
   }
-  printf("The bool array is %d bytes or %d bit.\n", BOOL_TAB_SIZE * 2,
-         (int)sizeof(bool) * 16 * BOOL_TAB_SIZE);
+  printf("The bool array is %d bytes or %d bit.\n", size * 2,
+         (int)sizeof(bool) * 16 * size);
 
   int pressed_size = 0;
-  bool_8 *bool_tab_pressed = bool_tab_pressed_init(bool_tab, BOOL_TAB_SIZE, &pressed_size);
+  bool_8 *bool_tab_pressed =
+      bool_tab_pressed_init(bool_tab, size, &pressed_size);
   if (!bool_tab_pressed) {
     print_err("Memory allocation failed !\n");
     return EXIT_FAILURE;
   }
-  printf("The bool_8 array is %d bytes or %d bit.\n", pressed_size , pressed_size * 8);
+  printf("The bool_8 array is %d bytes or %d bit.\n", pressed_size,
+         pressed_size * 8);
 
   free(bool_tab);
   free(bool_tab_pressed);
